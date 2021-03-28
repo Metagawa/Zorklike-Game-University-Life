ecc3d02e2c9ba8c061d3c89ce830f6f9f707a514
Jim Ryan <jimmih15@gmail.com>
2021-03-28 12:15:53 +0100

    scene change

--- University Life/uniZork.cpp
+++ University Life/uniZork.cpp
@@ -1,121 +1,103 @@
 #include "uniZork.h"
 #include "ui_uniZork.h"
 #include <QScrollBar>
-Zork::Zork(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Zork)
-{
-    ui->setupUi(this);
-    uniLife = new UniLife();
-    uniLife->printWelcome();
-    string welcomeText = "start \ninfo for help\n\n"+ uniLife->currentRoom->longDescription()+"\n";
-    ui->plainTextEdit->setPlainText(QString::fromStdString(welcomeText));
-    ui->plainTextEdit->setStyleSheet("color: blue;");
-    ui->plainTextEdit->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+Zork::Zork(QWidget *parent) : QMainWindow(parent), ui(new Ui::Zork) {
+  ui->setupUi(this);
+  uniLife = new UniLife();
+  uniLife->printWelcome();
+  string welcomeText = "start \ninfo for help\n\n" +
+                       uniLife->currentRoom->longDescription() + "\n";
+  ui->plainTextEdit->setPlainText(QString::fromStdString(welcomeText));
+  ui->plainTextEdit->setStyleSheet("color: blue;");
+  ui->plainTextEdit->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
-Zork::~Zork()
-{
-    delete ui;
-}
+Zork::~Zork() { delete ui; }
 
+void Zork::updatePositionAfterMoving() {
+  QString currentPlainText =
+      ui->plainTextEdit->toPlainText() + QString::fromStdString("\n") +
+      QString::fromStdString(uniLife->currentRoom->longDescription() + "\n");
 
-void Zork::updatePositionAfterMoving(){
-    QString currentPlainText = ui->plainTextEdit->toPlainText()+QString::fromStdString("\n")+QString::fromStdString(uniLife->currentRoom->longDescription()+"\n");
+  ui->plainTextEdit->setPlainText(currentPlainText);
 
+  // Puts the scroll bar to the bottom as setting the text moves it back to the
+  // top
+  ui->plainTextEdit->moveCursor(QTextCursor::End);
+  ui->plainTextEdit->ensureCursorVisible();
 
-   ui->plainTextEdit->setPlainText(currentPlainText);
-
-   // Puts the scroll bar to the bottom as setting the text moves it back to the top
-   ui->plainTextEdit->moveCursor(QTextCursor::End);
-   ui->plainTextEdit->ensureCursorVisible();
-
-   // Disables movement buttons based on if movement in that direction is possible or not
-   ui->northButton->setDisabled(false);
-   ui->southButton->setDisabled(false);
-   ui->eastButton->setDisabled(false);
-   ui->westButton->setDisabled(false);
-  if( uniLife->currentRoom->nextRoom("north")==NULL){
-      ui->northButton->setDisabled(true);
+  // Disables movement buttons based on if movement in that direction is
+  // possible or not
+  ui->northButton->setDisabled(false);
+  ui->southButton->setDisabled(false);
+  ui->eastButton->setDisabled(false);
+  ui->westButton->setDisabled(false);
+  if (uniLife->currentRoom->nextRoom("north") == NULL) {
+    ui->northButton->setDisabled(true);
   }
-  if( uniLife->currentRoom->nextRoom("south")==NULL){
-       ui->southButton->setDisabled(true);
+  if (uniLife->currentRoom->nextRoom("south") == NULL) {
+    ui->southButton->setDisabled(true);
   }
-  if( uniLife->currentRoom->nextRoom("east")==NULL){
-      ui->eastButton->setDisabled(true);
+  if (uniLife->currentRoom->nextRoom("east") == NULL) {
+    ui->eastButton->setDisabled(true);
   }
-  if( uniLife->currentRoom->nextRoom("west")==NULL){
-      ui->westButton->setDisabled(true);
+  if (uniLife->currentRoom->nextRoom("west") == NULL) {
+    ui->westButton->setDisabled(true);
   }
-
 
   // update image for room
-  if (uniLife->currentRoom->shortDescription()=="a"){
-      ui->imageBackground-> setPixmap(QPixmap(":/new/images/images/gym-room.png"));
-  }else if (uniLife->currentRoom->shortDescription()=="f"){
-      ui->imageBackground-> setPixmap(QPixmap(":/new/images/images/business-room.png"));
+  if (uniLife->currentRoom->shortDescription() == "a") {
+    ui->imageBackground->setPixmap(QPixmap(":/new/images/images/gym-room.png"));
+  } else if (uniLife->currentRoom->shortDescription() == "f") {
+    ui->imageBackground->setPixmap(
+        QPixmap(":/new/images/images/business-room.png"));
   }
-
 }
 
-void Zork::on_northButton_clicked()
-{
-    Command *northCommand = new Command("go", "north");
-    uniLife->processCommand(*northCommand);
-    updatePositionAfterMoving();
-
+void Zork::on_northButton_clicked() {
+  Command *northCommand = new Command("go", "north");
+  uniLife->processCommand(*northCommand);
+  updatePositionAfterMoving();
 }
 
-void Zork::on_eastButton_clicked()
-{
-    Command *northCommand = new Command("go", "east");
-    uniLife->processCommand(*northCommand);
-    updatePositionAfterMoving();
+void Zork::on_eastButton_clicked() {
+  Command *northCommand = new Command("go", "east");
+  uniLife->processCommand(*northCommand);
+  updatePositionAfterMoving();
 }
 
-void Zork::on_westButton_clicked()
-{
-    Command *northCommand = new Command("go", "west");
-    uniLife->processCommand(*northCommand);
-    updatePositionAfterMoving();
+void Zork::on_westButton_clicked() {
+  Command *northCommand = new Command("go", "west");
+  uniLife->processCommand(*northCommand);
+  updatePositionAfterMoving();
 }
 
-void Zork::on_southButton_clicked()
-{
-    Command *northCommand = new Command("go", "south");
-    uniLife->processCommand(*northCommand);
-    updatePositionAfterMoving();
+void Zork::on_southButton_clicked() {
+  Command *northCommand = new Command("go", "south");
+  uniLife->processCommand(*northCommand);
+  updatePositionAfterMoving();
 }
 
-void Zork::on_takeButton_clicked()
-{
-    Command *takeCommand = new Command("take", "x");
-    uniLife->processCommand(*takeCommand);
-
+void Zork::on_takeButton_clicked() {
+  Command *takeCommand = new Command("take", "x");
+  uniLife->processCommand(*takeCommand);
 }
 
-void Zork::on_infoButton_clicked()
-{
-    Command *infoCommand = new Command("info","");
-    uniLife->processCommand(*infoCommand);
-
+void Zork::on_infoButton_clicked() {
+  Command *infoCommand = new Command("info", "");
+  uniLife->processCommand(*infoCommand);
 }
 
-
-void Zork::on_mapButton_clicked()
-{
-    Command *mapCommand = new Command("map","");
-    uniLife->processCommand(*mapCommand);
+void Zork::on_mapButton_clicked() {
+  Command *mapCommand = new Command("map", "");
+  uniLife->processCommand(*mapCommand);
 }
 
-void Zork::on_putButton_clicked()
-{
-    Command *putCommand = new Command("put", "x");
-    uniLife->processCommand(*putCommand);
+void Zork::on_putButton_clicked() {
+  Command *putCommand = new Command("put", "x");
+  uniLife->processCommand(*putCommand);
 }
 
-void Zork::on_quitButton_clicked()
-{
-    QApplication::quit();
+void Zork::on_quitButton_clicked() { QApplication::quit(); }
 
-}
+void Zork::on_pushButton_clicked() { ui->stackedWidget->setCurrentIndex(0); }
--- University Life/uniZork.h
+++ University Life/uniZork.h
@@ -1,45 +1,47 @@
 #ifndef UNIZORK_H
 #define UNIZORK_H
 
-#include <QMainWindow>
 #include "uniLife.h"
+#include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Zork; }
+namespace Ui {
+class Zork;
+}
 QT_END_NAMESPACE
 
-class Zork : public QMainWindow
-{
-    Q_OBJECT
+class Zork : public QMainWindow {
+  Q_OBJECT
 
 public:
-    Zork(QWidget *parent = nullptr);
-    ~Zork();
+  Zork(QWidget *parent = nullptr);
+  ~Zork();
 
 private slots:
 
-    void on_northButton_clicked();
+  void on_northButton_clicked();
 
-    void on_eastButton_clicked();
+  void on_eastButton_clicked();
 
-    void on_westButton_clicked();
+  void on_westButton_clicked();
 
-    void on_southButton_clicked();
+  void on_southButton_clicked();
 
-    void on_takeButton_clicked();
+  void on_takeButton_clicked();
 
-    void on_infoButton_clicked();
+  void on_infoButton_clicked();
 
-    void on_mapButton_clicked();
+  void on_mapButton_clicked();
 
-    void on_putButton_clicked();
+  void on_putButton_clicked();
 
-    void on_quitButton_clicked();
+  void on_quitButton_clicked();
+
+  void on_pushButton_clicked();
 
 private:
-    Ui::Zork *ui;
-    UniLife *uniLife;
-    void updatePositionAfterMoving();
-
+  Ui::Zork *ui;
+  UniLife *uniLife;
+  void updatePositionAfterMoving();
 };
 #endif // UNIZORK_H
