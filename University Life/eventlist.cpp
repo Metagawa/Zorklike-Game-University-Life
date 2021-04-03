3eb9d1809251514e5eaf49cfcdd687f1d9591e70
TomC17 <tommiecarey17@gmail.com>
2021-04-03 21:54:39 +0100

    template event!

--- University Life/eventlist.cpp
+++ University Life/eventlist.cpp
@@ -2,24 +2,42 @@
 #include "event.h"
 #include <vector>
 EventList::EventList() {
-  Event *head1 = new Event(
-      "Welcome to the Library", "Say \'Bye\'", "Leave without saying anything",
-      "This event is now over", "There is an event", 5, "", false);
-  head1->leftOption =
-      new Event("some text", "left option", "right option", true);
+    Event *ToplevelDogScene = new Event(
+                "You see a small dog lying in the road", "Ignore it, it's not your problem.", "Run over and see if it's ok",
+                "I helped the doggy out.", "Is there something in the road?",0,"",false);
 
+    Event *MidlevelDogSceneA = new Event(
+                "You ignore the dog, like a heartless monster. You hear him whine softly.","Keep walking.","He needs help, go help him!",
+                false);
 
-  Event *head2 = new Event(
-      "Welcome to the kitchen", "Say \'Bye\'", "Leave without saying anything",
-      "This event is now over", "There is an event", 5, "", false);
-  head2->rightOption =
-      new Event("some text", "left option", "right option", true);
-  head2->leftOption =
-      new Event("some text", "left option", "right option", true);
+    Event *MidlevelDogSceneB = new Event(
+                "You ran over and grabbed the dog, he's ok he's just hungry.", "Take him to raps for some food?","Give him your lunch so you have an excuse to go to raps?",false);
 
-  head1->rightOption=head2;
+    Event *BottomlevelDogSceneA = new Event(
+                "You've walked out of earshot of the poor whimpering dog.","Go to raps?","Go to class?",true
+                );
 
-  events.push_back(*head1);
+            Event *BottomlevelDogSceneB = new Event(
+                        "Showing that you actually do have a human heart, you race to the dog's aid. You see that he's emaciated and needs food badly.","Take him to raps?","Give him your lunch?",true
+                        );
+
+            Event *BottomlevelDogSceneC = new Event(
+                       "You bought some chicken and water for the dog to eat, grabbing a wrap for yourself as you're at it. The dog barks cheerfully at you and runs off after having his fill.","Go to class?","Go home?",true
+                        );
+
+            Event *BottomlevelDogSceneD = new Event(
+                        "You give the dog your lunch. Your stomach mourns the loss of its anticipated breakfast roll, but you console yourself with the promise of a wrap later. The dog seems really happy to have a good meal and runs off after he's finished it.","Go home?","Go to class?",true
+                        );
+
+            ToplevelDogScene->leftOption=MidlevelDogSceneA;
+            ToplevelDogScene->rightOption=MidlevelDogSceneB;
+            MidlevelDogSceneA->leftOption=BottomlevelDogSceneA;
+            MidlevelDogSceneA->rightOption=BottomlevelDogSceneB;
+            MidlevelDogSceneB->leftOption=BottomlevelDogSceneC;
+            MidlevelDogSceneB->rightOption=BottomlevelDogSceneD;
+
+            events.push_back(*ToplevelDogScene);
+
 
 }
 
