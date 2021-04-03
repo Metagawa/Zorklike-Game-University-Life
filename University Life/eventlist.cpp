1e3d46b52eaa45fc30a6c4d107cd1d3ebe85a764
TomC17 <tommiecarey17@gmail.com>
2021-04-03 21:31:50 +0100

    Update eventlist.cpp

--- University Life/eventlist.cpp
+++ University Life/eventlist.cpp
@@ -5,12 +5,22 @@ EventList::EventList() {
   Event *head1 = new Event(
       "Welcome to the Library", "Say \'Bye\'", "Leave without saying anything",
       "This event is now over", "There is an event", 5, "", false);
-  head1->rightOption =
-      new Event("some text", "left option", "right option", true);
   head1->leftOption =
       new Event("some text", "left option", "right option", true);
 
+
+  Event *head2 = new Event(
+      "Welcome to the kitchen", "Say \'Bye\'", "Leave without saying anything",
+      "This event is now over", "There is an event", 5, "", false);
+  head2->rightOption =
+      new Event("some text", "left option", "right option", true);
+  head2->leftOption =
+      new Event("some text", "left option", "right option", true);
+
+  head1->rightOption=head2;
+
   events.push_back(*head1);
+
 }
 
 Event *EventList::getRandomEvent() {
