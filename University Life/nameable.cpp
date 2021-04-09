959d781ea511da1845e72743608f1687196a55ba
TomC17 <tommiecarey17@gmail.com>
2021-04-09 13:28:59 +0100

    virtual function fix

--- University Life/nameable.cpp
+++ University Life/nameable.cpp
@@ -1,6 +1,5 @@
 #include "nameable.h"
 #include <string>
 using namespace std;
-string Nameable::getName() { return name; }
 
 void Nameable::setName(string name) { this->name = name; }
--- University Life/nameable.h
+++ University Life/nameable.h
@@ -4,7 +4,9 @@
 using namespace std;
 class Nameable {
 public:
-  virtual string getName();
+    virtual string getName() {
+        return name;
+    };
   void setName(string name);
 
 protected:
