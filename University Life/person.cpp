3b37d6eef039d331e5b8d1354f9dd201977a1df1
Jim Ryan <jimmih15@gmail.com>
2021-04-10 22:00:03 +0100

    Update person.cpp

--- University Life/person.cpp
+++ University Life/person.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+// this constructor calls the method setName from the nameable abstract class
 Person::Person(string name) { setName(name); }
+// implements the getName() virtual function from the nameable abstract class
 string Person::getName() { return this->name; }
 
+// prints a greeting with the characters name to the console
 void Person::greet() { cout << "Hello my name is " << name << endl; }
