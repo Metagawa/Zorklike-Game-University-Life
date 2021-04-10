46f142cbd612566211b1111eb71f00fe7c49e29d
TomC17 <tommiecarey17@gmail.com>
2021-04-10 22:01:54 +0100

    commented main
    Co-Authored-By: Jim Ryan <jimmih15@gmail.com>

--- University Life/main.cpp
+++ University Life/main.cpp
@@ -4,21 +4,26 @@
 #include <QSplashScreen>
 #include <QTimer>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+  //sets a new QApplication with the passed arguments
   QApplication a(argc, argv);
+  //creates a pixmap for the splash screen and sets it
   QPixmap pixmap(":/images/misc/splash.png");
   QSplashScreen splash(pixmap);
+  //displays the splash screen on launch for 1.5 seconds
   splash.show();
-  QTimer::singleShot(1500, &splash,
-                     &QWidget::close); // keep displayed for 5 seconds
+  QTimer::singleShot(1500, &splash, &QWidget::close);
+  //creates a new Zork window
   Zork w;
-
-  // splash.finish(&w);
+  //locks the thread so that the game window doesn't appear behind the splash screen
   QMutex mut;
   mut.lock();
   mut.tryLock(1500);
-  mut.unlock(); // I am not sure if this is a necessity
+  //unlocks the thread and shows the game window
+  mut.unlock();
   w.show();
+  //sets a custom cursor
   a.setOverrideCursor(QCursor(QPixmap(":/images/misc/black-biro.png")));
   return a.exec();
 }
