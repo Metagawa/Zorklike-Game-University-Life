ef97a4c3faaca5667c43f4920e37a2c5804e5979
Jim Ryan <jimmih15@gmail.com>
2021-04-10 21:53:47 +0100

    nameable and ageable comments

--- University Life/ageable.cpp
+++ University Life/ageable.cpp
@@ -1,5 +1,7 @@
 #include "ageable.h"
 
+// increases the age
 void Ageable::getOlder() { this->age++; }
 
+// sets the current age
 void Ageable::setAge(int age) { this->age = age; }
--- University Life/nameable.cpp
+++ University Life/nameable.cpp
@@ -2,4 +2,5 @@
 #include <string>
 using namespace std;
 
+// sets the name to the string name
 void Nameable::setName(string name) { this->name = name; }
