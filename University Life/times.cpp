6eabb669cbd7478b51d13452f624fec053b264ab
Jim Ryan <jimmih15@gmail.com>
2021-04-10 21:48:03 +0100

    Update times.cpp

--- University Life/times.cpp
+++ University Life/times.cpp
@@ -8,16 +8,27 @@ namespace timespace {
 string season = "Autumn";
 } // namespace timespace
 
+// gets the current season
 string Times::getSeason() { return timespace::season; }
 
+// Constructor which sets the current time to 9am and the current day to 0
 Times::Times() {
   setTime(9);
   setDayNum(0);
   rain = (rand() % 2) != 0;
 }
 
+// gets the current day as an int
 int Times::getDayNum() { return this->dayNum; }
+
+// gets the current time
 int Times::getTime() { return this->time; }
+
+// advances the time by 1 hour. If it is a new day it will set the time back and
+// increment the current day, furthermore the rain boolean is updated on every
+// new day and every evening
+// returns a boolean if there is a new day (This info is needed in unizork.cpp
+// as we need to know when to make a new day of the week tab
 bool Times::advanceTime() {
   time += 1;
   if (time >= NUM_HOURS_IN_DAY) {
@@ -30,8 +41,14 @@ bool Times::advanceTime() {
   }
   return false;
 }
+
+// returns whether or not it is daytime (before 6pm)
 bool Times::isDayTime() { return time < 18; }
+
+// reutrns whether or not it is raining
 bool Times::isRaining() { return this->rain; }
+
+// returns the current time as a string for the lcd display
 string Times::getTimeString() {
   if (getTime() >= 9) {
     return "0" + to_string(getTime()) + ":00";
@@ -40,6 +57,8 @@ string Times::getTimeString() {
   }
 }
 
+// overloaded method which returns the day of the week as a string when given an
+// int
 string Times::getDayString(int i) {
   switch (i) {
   case 0:
@@ -63,8 +82,13 @@ string Times::getDayString(int i) {
   }
 }
 
+// returns the current day of the week as a string
 string Times::getDayString() { return getDayString(getDayNum()); }
+
+// set the current time
 void Times::setTime(int newtime) { this->time = newtime; }
+
+// set the current day
 void Times::setDayNum(int newday) { this->dayNum = newday; }
 
 // postfix ++ operator overrided
